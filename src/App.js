c72cf6b65a66cbf329bbe0e9cd54cfa4b24fb31f
AarMagic <aarcarpas@gmail.com>
2024-07-19 00:41:11 +0200

    Creado Cofiguracion y asignacion de temas

--- src/App.js
+++ src/App.js
@@ -6,17 +6,48 @@ import { useEffect, useState } from 'react';
 
 function App() {
   const [taskListState, setTaskListState] = useState([]);
+  const [actualThemeState, setActualThemeState] = useState([]);
 
-  useEffect(() => {    
+  const themes = [
+    { primary: '#4D0090', secondary: '#01FF71' },
+    { primary: '#4d07a1', secondary: '#24f9b6' },
+    { primary: '#3e0057', secondary: '#0cf0a7' },
+    { primary: '#FFF3FE', secondary: '#6449B7' },
+    { primary: '#cfef78', secondary: '#263653' },
+    { primary: '#205965', secondary: '#7afcd3' },
+    { primary: '#DB7A9C', secondary: '#421A44' },
+    { primary: '#5a2883', secondary: '#edffb3' },
+    { primary: '#0b3c81', secondary: '#75ef1a' },
+    { primary: '#59c355', secondary: '#0e0417' },
+    { primary: '#EAEAEA', secondary: '#4A1F43' },
+    { primary: '#96f5ec', secondary: '#182348' },
+    { primary: '#00569b', secondary: '#fcb9c7' }
+  ];
+  
+  useEffect(() => {
+    let theme = JSON.parse(localStorage.getItem("theme"));
     let tasks = JSON.parse(localStorage.getItem("taskList"));
-      if (tasks !== null) {
-        setTaskListState(tasks);
-      }
+    
+    if (theme !== null) {
+      setActualThemeState(theme);
+  } else {
+      setActualThemeState(themes[0]);
+  }
+
+    if (tasks !== null) {
+      setTaskListState(tasks);
+    }
   }, [])
+
+  useEffect(() => {
+    document.documentElement.style.setProperty("--primary", actualThemeState.primary)
+    document.documentElement.style.setProperty("--secondary", actualThemeState.secondary)
+  }, [actualThemeState.primary, actualThemeState.secondary, setActualThemeState])
+
 
   return (
     <div className="App">
-      <CustomBar taskList={taskListState} setTaskListState={setTaskListState} />
+      <CustomBar taskList={taskListState} setTaskListState={setTaskListState} actualThemeState={actualThemeState} setActualThemeState={setActualThemeState} themes={themes} />
       <main>
         <section className="cards">
           {taskListState.length > 0 && (
--- src/components/CustomBar.js
+++ src/components/CustomBar.js
@@ -1,27 +1,42 @@
 import React, { useState } from 'react'
 import { FormComponent } from './FormComponent';
+import { Config } from './Config';
 
-export const CustomBar = ({ taskList, setTaskListState }) => {
-    const [form, setFormState] = useState(false);
+export const CustomBar = ({ taskList, setTaskListState, actualThemeState, setActualThemeState, themes }) => {
+    const [formState, setFormState] = useState(false);
+    const [configState, setConfigState] = useState(false);
     const [activeButton, setActiveButtonState] = useState("");
     const showForm = (e, newState) => {
         setActiveButtonState(newState)
         setFormState(true)
     }
+
+    const showConfig = () => {
+        setConfigState(true);
+    }
+
     return (
         <div className="custom-bar">
-            {(form === true && <FormComponent
+            {(formState === true && <FormComponent
                 setFormState={setFormState}
                 activeButton={activeButton}
                 setActiveButtonState={setActiveButtonState}
                 taskList={taskList}
                 setTaskListState={setTaskListState} />)}
+            {
+                configState === true && <Config
+                    actualThemeState={actualThemeState}
+                    setActualThemeState={setActualThemeState}
+                    setConfigState={setConfigState}
+                    themes={themes}
+                />
+            }
             <div className="bar add-bar" onClick={e => showForm(e, "crear")}>
                 <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px">
                     <path d="M440-440H200v-80h240v-240h80v240h240v80H520v240h-80v-240Z" />
                 </svg>
             </div>
-            <div className="bar edit-bar">
+            <div className="bar edit-bar" onClick={showConfig}>
                 <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed">
                     <path
                         d="M440-120v-240h80v80h320v80H520v80h-80Zm-320-80v-80h240v80H120Zm160-160v-80H120v-80h160v-80h80v240h-80Zm160-80v-80h400v80H440Zm160-160v-240h80v80h160v80H680v80h-80Zm-480-80v-80h400v80H120Z" />
